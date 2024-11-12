0192c0e78fccbf1cfdc8fe45927ae0898cfa8cae
Anthony Ikeda <anthony.ikeda@gmail.com>
2024-11-11 21:13:19 -0800

    Trying to get unit tests working. Otherwise I think this persists to the database as expected.

--- file_service.py
+++ file_service.py
@@ -5,15 +5,31 @@ from marshmallow import Schema, fields
 
 logger = logging.getLogger(__name__)
 
+# CONNECT_URL = "psycopgdbname=library_db user=librarian passwd=letmein"
+CONNECT_URL = "postgresql://librarian:letmein@localhost:5432/library_db"
+
+
 class FileMeta:
     def __init__(self, file_id, file_name, content):
         self.file_id = file_id
         self.file_name = file_name
         self.content = content
 
+    def file_id( self ):
+        return self.file_id
+
 class FileColl:
+
     def __init__(self, files):
-        self.files = files
+        self._files = files
+    
+    @property
+    def files( self ):
+        return self._files
+    
+    @files.setter
+    def files(self, new_files):
+        self._files = new_files
 
 
 class FileMetaSchema(Schema):
@@ -28,10 +44,20 @@ class FileCollSchema(Schema):
 
 def saveFiles(file_metadata: FileColl):
     items = []
-    for rec in file_metadata["files"]:
-        logger.info(f'${rec["name"]}, ${rec["content"]}')
-        item = FileMeta(file_id=1,file_name=rec["name"], content=rec["content"])
-        items.append(item)
-    saved = FileColl(files=items)
+
+    with connectDB() as conn:
+        with conn.cursor() as cur:
+            for rec in file_metadata["files"]:
+                logger.info(f'${rec["name"]}, ${rec["content"]}')
+                cur.execute('insert into files (file_name,content) values (%s, %s) returning file_id', (rec["name"], rec["content"]))
+                new_id = cur.fetchone()[0]
+                print(f'New id is {new_id}')
+                item = FileMeta(file_id=new_id,file_name=rec["name"], content=rec["content"])
+
+                items.append(item)
+        saved = FileColl(files=items)
+
     return saved
 
+def connectDB():
+    return psycopg.connect(CONNECT_URL)
--- tests.py
+++ tests.py
@@ -1,9 +1,42 @@
 import unittest
-from file_service import saveFiles, FileMetaSchema, FileCollSchema
+from file_service import saveFiles, FileCollSchema, connectDB
+from unittest.mock import MagicMock, Mock
+from unittest.mock import patch
 
 class TestSaveFiles(unittest.TestCase):
-    def test_saveFiles(self):
-        
+
+    @patch('psycopg.connect')
+    def test_database_connection(self, mock_connect):
+
+        mock_cursor = MagicMock()
+        mock_cursor.__enter__.return_value.fetch_one.return_value = ['1']
+        mock_cursor.__exit__ = MagicMock()
+
+        mock_connect.return_value.cursor.return_value = mock_cursor
+
+        files = '{ "files": [{ "name" : "file_1.txt", "content": "This is the body"}]}'
+
+        schema = FileCollSchema()
+        data = schema.loads(files)
+
+        results = saveFiles(data)
+        print(results)
+        print(results.files[0].file_id)
+        self.assertEqual(results.files[0].file_id, 1, "Identifier doesn't match")
+
+    @patch('psycopg.connect')
+    def test_saveFiles(self, mock_connect):
+        mock_cursor = MagicMock()
+        mock_cursor.__enter__.return_value.fetch_one.return_value = {
+            "file_id": 1,
+            "file_name" : "file1.txt",
+            "content" : "content of file 1"
+        }
+
+        mock_cursor.__exit__ = MagicMock()
+
+        mock_connect.return_value.cursor.return_value = mock_cursor
+
         files = '{ "files": [{ "name" : "file_1.txt", "content": "This is the body"}]}'
 
         schema = FileCollSchema()
