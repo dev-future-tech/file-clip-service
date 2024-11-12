9397bbaacf0890e90448812971a0152ccf4f0a20
Anthony Ikeda <anthony.ikeda@gmail.com>
2024-11-11 21:16:00 -0800

    Added database schema

---
+++ database.sql
@@ -0,0 +1,9 @@
+create role librarian with login password 'letmein';
+create database library_db with owner librarian;
+
+create table files(
+    file_id bigint generated always as identity not null,
+    file_name varcjar(120),
+    content TEXT,
+    primary key (file_id)
+);
